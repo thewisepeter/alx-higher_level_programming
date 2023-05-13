6f7a631e74f0ef1e2bcbb8a53c08faaaf85d0924
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-13 10:27:56 +0300

    added check for integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,7 @@
 def print_list_integer(my_list=[]):
     """ function that prints all integers in my_list"""
     for x in my_list:
-        print("{}".format(x), end="\n")
+        if type(x) == int:
+            print("{}".format(x))
+        else:
+            print("Non-integer value")
