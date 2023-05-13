ac3a65956aebe39ca13ddb4fe192edf2664550f0
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-13 10:31:59 +0300

    added format specifier

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,4 @@
 def print_list_integer(my_list=[]):
     """ function that prints all integers in my_list"""
     for x in my_list:
-        if type(x) == int:
-            print("{}".format(x))
-        else:
-            print("Non-integer value")
+        print("{:d}".format(x))
