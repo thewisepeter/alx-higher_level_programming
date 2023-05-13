d2a62ec2d361b84e30933a18baa94416507f1ae1
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-13 10:42:25 +0300

    fixed condition for idx > length

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,7 +8,7 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0:
         return my_list
-    elif idx > length:
+    elif idx > (length - 1):
         return my_list
     else:
         new_list_1 = new_list.pop(idx)
