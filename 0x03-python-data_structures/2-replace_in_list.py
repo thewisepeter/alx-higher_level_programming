5f74106eacd27e5d3f27fe43b1434a2229d16a44
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-13 10:35:58 +0300

    fixed condition for idx > length

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,7 @@ def replace_in_list(my_list, idx, element):
 
     if idx < 0:
         return my_list
-    elif idx > length:
+    elif idx > (length - 1):
         return my_list
     else:
         new_list = my_list.pop(idx)
