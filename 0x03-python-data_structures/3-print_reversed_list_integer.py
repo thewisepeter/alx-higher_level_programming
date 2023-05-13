e3a64cc41b0fb2db1800f302c664ba14680e5bca
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-13 10:37:00 +0300

    added format specifier

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     """function that prints all integers of a list, in reverse order"""
     for x in reversed(my_list):
-        print("{}".format(x))
+        print("{:d}".format(x))
