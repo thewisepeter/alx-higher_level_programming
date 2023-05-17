27a1b0ad687d06c215b3ccf5b0742cfd4c4fe529
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-17 21:36:59 +0300

    function that replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     """function that prints a dictionary by ordered keys."""
-    sorted_keys = sorted(a_dictionary.keys())
+    sorted_keys = sorted(a_dictionary.key())
 
     for key in sorted_keys:
         print("{}: {}".format(key, a_dictionary[key]))
