9976583804f8241e04f98397420c2db24031e271
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-18 01:10:04 +0300

    two C functions that print some basic info about Python lists and Python bytes objects.

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - prints python list
+ * @p: Python Object
+ *
+ * Return: Nothing
+ */
+void print_python_list(PyObject *p)
+{
+	(void)p;
+}
+
+/**
+ * print_python_bytes - prints python bytes
+ * @p: Python Object
+ *
+ * Return: Nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+	(void)p;
+}
