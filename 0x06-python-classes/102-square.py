7bbc0cde49f6e07d4977bc2a78976b3e196b79d7
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-31 22:13:15 +0300

    changed attribute names

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,6 +5,7 @@
 class Square:
     """builds a square
        finds the area"""
+
     def __init__(self, size=0):
         """initializes a square of length size"""
         self.__size = size
@@ -15,32 +16,32 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """sets size of square"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """ calculates area of square"""
         return self.__size ** 2
 
-    def __eq__(self, other):
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        return self.area() != other.area
+    def __le__(self, other):
+        return self.area() <= other.area()
 
     def __lt__(self, other):
         return self.area() < other.area
 
-    def __le__(self, other):
-        return self.area() <= other.area
+    def __ge__(self, other):
+        return self.area() >= other.area
+
+    def __ne__(self, other):
+        return self.area() != other.area
 
     def __gt__(self, other):
         return self.area() > other.area
 
-    def __ge__(self, other):
-        return self.area() >= other.area
+    def __eq__(self, other):
+        return self.area() == other.area
