6d62d7ab6a93894acc8158640f47482500c6a616
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-31 22:18:42 +0300

    reverted back a step

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,7 +5,6 @@
 class Square:
     """builds a square
        finds the area"""
-
     def __init__(self, size=0):
         """initializes a square of length size"""
         self.__size = size
@@ -16,32 +15,32 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, size):
         """sets size of square"""
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     def area(self):
         """ calculates area of square"""
         return self.__size ** 2
 
-    def __le__(self, other):
-        return self.area() <= other.area()
-
-    def __lt__(self, other):
-        return self.area() < other.area
-
-    def __ge__(self, other):
-        return self.area() >= other.area
+    def __eq__(self, other):
+        return self.area() == other.area()
 
     def __ne__(self, other):
         return self.area() != other.area
 
+    def __lt__(self, other):
+        return self.area() < other.area
+
+    def __le__(self, other):
+        return self.area() <= other.area
+
     def __gt__(self, other):
         return self.area() > other.area
 
-    def __eq__(self, other):
-        return self.area() == other.area
+    def __ge__(self, other):
+        return self.area() >= other.area
