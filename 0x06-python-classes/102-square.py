ded0b85d8b16469fff8f2eba123ec2520aa86b98
Peter Ochan <rempeterwisdom@gmail.com>
2023-05-31 19:54:06 +0300

    checks equality of tow squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """builds a square
+       finds the area"""
+    def __init__(self, size=0):
+        """initializes a square of length size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """gets size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets size of square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ calculates area of square"""
+        return self.__size ** 2
+
+    def __equal__(self, other):
+        return self.area() == other.area()
+
+    def __notequal__(self, other):
+        return self.area() != other.area
+
+    def __lessthan__(self, other):
+        return self.area() < other.area
+
+    def __lessthanequal__(self, other):
+        return self.area() <= other.area
+
+    def __greaterthan__(self, other):
+        return self.area() > other.area
+
+    def __greaterthanequal__(self, other):
+        return self.area() >= other.area
