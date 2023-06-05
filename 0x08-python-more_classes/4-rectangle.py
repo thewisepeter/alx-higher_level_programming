80fde144b2177a50704a659e76b6b53384210ff0
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-05 15:36:28 +0300

    added __repr__ eval

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
- """Reactangle Definition"""
+"""Reactangle Definition"""
 
 
 class Rectangle:
     """ Rectangle Definition"""
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -14,13 +15,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """ sets width """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -28,13 +29,13 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """ sets height """
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
 
     def area(self):
         """gets area of rectangle"""
@@ -44,13 +45,13 @@ class Rectangle:
         """gets perimeter of rectangle"""
         if (self.__height == 0 or self.__width == 0):
             return 0
-        return 2 * (self.__height + self.__width)
+        return (2 * (self.__height + self.__width))
 
     def __str__(self):
         """returns string representation of rectangle"""
         if (self.__height == 0 or self.__width == 0):
-            return ()
+            return ("")
         return ("\n".join(["#" * self.__width] * self.__height))
 
     def __repr__(self):
-        return 
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
