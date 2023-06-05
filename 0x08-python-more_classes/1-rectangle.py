2217f535f536ccb67c02d0971b8a56c40ddbbb2a
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-05 14:05:37 +0300

    changed from height/width to value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,24 +14,24 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """ sets width """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
-        """get width"""
+        """get height"""
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """ sets height """
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
