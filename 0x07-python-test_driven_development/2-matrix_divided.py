d01cfadf20e95eff72759abf72a8ce4ca7dca7b9
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-06 14:51:58 +0300

    function that divides all elements of a matrix and its doctest

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module with a function that divides all elements of a matrix """
+
+def matrix_divided(matrix, div):
+    """function that divides all elements of a matrix"""
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)")
+    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix of integers/floats")
+    row_sizes = [len(row) for row in matrix]
+    if not all(size == row_sizes[0] for size in row_sizes):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise TypeError("division by zero")
+
+    return([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
