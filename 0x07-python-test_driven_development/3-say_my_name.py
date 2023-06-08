9bc423cef77cac8c2f782716d00b57b7c489b86d
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-08 15:34:10 +0300

    documented function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,8 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """function that prints My name is <first name> <last name>"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
