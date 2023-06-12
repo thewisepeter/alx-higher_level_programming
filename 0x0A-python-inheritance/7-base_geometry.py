ca0287ed6d91b91b77efe64ed036a62de5119000
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-12 20:45:14 +0300

    changed out isinstance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,14 +3,15 @@
 
 
 class BaseGeometry:
-    """empty class"""
+    """class of base geometry"""
+
     def area(self):
         """gets area of figure"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """validates value"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
