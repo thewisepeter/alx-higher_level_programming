3fe769dece5e464e929df60cba0049081941510f
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-13 22:48:30 +0300

    removed encoding

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """this function writes an Object to a
         text file in JSON representation"""
-        with open(filename, 'w', encoding="utf-8") as f:
+        with open(filename, 'w') as f:
             json.dump(my_obj, f)
