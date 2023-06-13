2f4aac3dd297e3c8e35cc74859c6e76fd166695e
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-13 23:17:26 +0300

    module that uses various functions to append text to python list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this is a module that uses various functions to
+    append text to python list"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, "add_item.json")
