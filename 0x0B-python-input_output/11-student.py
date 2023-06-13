d0222a2b2681978adfa02d15a87031278d08fd3e
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-13 23:51:04 +0300

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" this is a module with a  student class"""
+
+
+class Student:
+    """this is a student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary representation of Student"""
+        if attrs is None:
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in
+                attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        for attr, value in json.items():
+            setattr(self, attr, value)
