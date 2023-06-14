a19960e0357874b25df0cc79a2a20c8529f2d7dc
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-14 16:07:29 +0300

    changed str return

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,10 +14,8 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """returns area of rectangle"""
-        return self.area
+        return self.__width * self.__height
 
     def __str__(self):
         """returns string representation of Rectangle"""
-        string = "[" + str(self.__class__.__name__) + "]"
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        return f"[Rectangle] {self.__width}/{self.__height}"
