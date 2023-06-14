fceac97adb02bbbd33161a0a2f1a387b7d49f610
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-14 15:55:46 +0300

    Full rectangle: added str implememtation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-"""module with an empty class"""
-
-
-class BaseGeometry:
-    """class of base geometry"""
-
-    def area(self):
-        """gets area of figure"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""module with Recatangle class that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -25,3 +11,13 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """returns string representation of Rectangle"""
+        string = "[" + str(self.__class__.__name__) + "]"
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
