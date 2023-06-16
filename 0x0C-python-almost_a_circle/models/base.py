4cf36d3b0894970c4e224b4deeaca646c8408b32
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-16 16:15:11 +0300

    the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module with the base class"""
+
+
+class Base:
+    """base of all other classes
+        prevents code duplication
+        and by extension bugs"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialises the Base """
+        if id is not None:
+            self.__id = id
+        else:
+            Base.__nb_objects += 1
+            self.__id = Base.__nb_objects
