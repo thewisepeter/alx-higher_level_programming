a32511c12a22634def751bc90baf7a3348e2d3c3
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-17 12:26:36 +0300

    added cordinates to display func

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,9 @@ class Rectangle(Base):
 
     def display(self):
         """displays the rectangle using #"""
+        print("\n" * self.__y, end="")
         for x in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """string representation of the rectangle"""
