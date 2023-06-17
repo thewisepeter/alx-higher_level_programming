689a6b4b62dde991998074c1db3201c035a19747
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-17 11:37:47 +0300

    till Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,7 +20,12 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
-        self.__width = width
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
 
     @property
     def height(self):
@@ -28,7 +33,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
-        self.__height = height
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
 
     @property
     def x(self):
@@ -36,7 +46,12 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
-        self.__x = x
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
 
     @property
     def y(self):
@@ -44,4 +59,18 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
-        self.__y = y
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """displays the rectangle using #"""
+        for x in range(self.__height):
+            print("#" * self.__width)
