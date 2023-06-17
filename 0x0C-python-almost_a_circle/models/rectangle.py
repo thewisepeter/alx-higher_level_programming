6b9763dd3cc5718ce905da07dd8ba815e9de8dc6
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-17 19:32:02 +0300

    Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,32 @@ class Rectangle(Base):
                 f"{self.__x}/{self.__y} - "
                 f"{self.__width}/{self.__height}"
                 )
+
+    def update(self, *args, **kwargs):
+        """assigns arguments to attributes"""
+        if args:
+            if len(args) >= 1:
+                self.__id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
