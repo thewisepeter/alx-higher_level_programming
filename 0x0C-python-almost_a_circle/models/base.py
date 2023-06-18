d78a179ec645beaf2987b6f1c278d656dcb87d8d
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-18 14:25:58 +0300

    Dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,6 +44,7 @@ class Base:
         """eturns the list of the JSON string representation json_string"""
         return json.loads(json_string)
 
+    @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
