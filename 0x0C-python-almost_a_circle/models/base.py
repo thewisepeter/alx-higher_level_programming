592e7a6d01b804aaa389558788cbaadfc9ae626c
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-19 23:46:57 +0300

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,16 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_objs = []
+
+        file_name = cls.__name__ + ".json"
+        json_data = cls.to_json_string([obj.to_dictionary()
+                                        for obj in list_objs])
+
+        with open(file_name, "w") as a_file:
+            a_file.write(json_data)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import os
 
 
 class TestBaseClass(unittest.TestCase):
@@ -46,6 +47,47 @@ class TestBaseClass(unittest.TestCase):
         list_dicts = None
         expected_json = '[]'
         self.assertEqual(Base.to_json_string(list_dicts), expected_json)
+
+    def tearDown(self):
+        """Remove created files after each test"""
+        file_names = ["Rectangle.json", "Square.json"]
+        for file_name in file_names:
+            if os.path.exists(file_name):
+                os.remove(file_name)
+
+    def test_save_to_file_rectangle(self):
+        """Test save_to_file method with Rectangle objects"""
+        Rectangle._Base__nb_objects = 0
+
+        r1 = Rectangle(10, 20, 1, 1, id=1)
+        r2 = Rectangle(5, 5, 2, 2, id=2)
+        rectangles = [r1, r2]
+        Rectangle.save_to_file(rectangles)
+
+        filename = "Rectangle.json"
+        self.assertTrue(os.path.exists(filename))
+
+        with open(filename, 'r') as file:
+            content = file.read()
+            self.assertEqual(content, '[{"x": 1, "y": 1, "id": 1, "height": 20, "width": 10}, '
+                                       '{"x": 2, "y": 2, "id": 2, "height": 5, "width": 5}]')
+
+    def test_save_to_file_square(self):
+        """Test save_to_file method with Square objects"""
+        Square._Base__nb_objects = 0
+
+        s1 = Square(10, 1, 1, 1)
+        s2 = Square(5, 2, 2, 2)
+        squares = [s1, s2]
+        Square.save_to_file(squares)
+
+        filename = "Square.json"
+        self.assertTrue(os.path.exists(filename))
+
+        with open(filename, 'r') as file:
+            content = file.read()
+            self.assertEqual(content, '[{"id": 1, "x": 1, "size": 10, "y": 1}, '
+                                       '{"id": 2, "x": 2, "size": 5, "y": 2}]')
 
 
 if __name__ == '__main__':
