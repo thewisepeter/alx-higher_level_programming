02bc504a1f3e58689077b541dbe67f1afc9a5f7f
Peter Ochan <rempeterwisdom@gmail.com>
2023-06-20 16:17:49 +0300

    JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module of the class Base"""
 import json
+import csv
 
 
 class Base:
@@ -69,6 +70,36 @@ class Base:
                 json_str = a_file.read()
                 obj_list = cls.from_json_string(json_str)
                 instances = [cls.create(**obj) for obj in obj_list]
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        #saves object instance to cls file
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            if list_objs is not None:
+                for obj in list_objs:
+                    if cls.__name__ == "Rectangle":
+                        writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                    elif cls.__name__ == "Square":
+                        writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r") as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        instance = cls.create(width=int(row[1]), height=int(row[2]), x=int(row[3]), y=int(row[4]))
+                    elif cls.__name__ == "Square":
+                        instance = cls.create(size=int(row[1]), x=int(row[2]), y=int(row[3]))
+                    instances.append(instance)
                 return instances
         except FileNotFoundError:
             return []
