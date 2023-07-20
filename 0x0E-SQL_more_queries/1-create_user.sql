e74a2725bc06f78514284749c48862dec661f729
Peter Ochan <rempeterwisdom@gmail.com>
2023-07-20 19:38:25 +0300

    creates the MySQL server user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- creates the MySQL server user user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
