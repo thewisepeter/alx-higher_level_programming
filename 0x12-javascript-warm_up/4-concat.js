912d1a96c41e9cf247da9446d21fbf12264198c7
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-14 23:19:39 +0300

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 2) {
+  console.log(`${args[0]} is + $args[1]}`);
+} else if (args.length === 1) {
+  console.log(`${arg[0]} is undefined`);
+} else if (args.length === 0) {
+  console.log("undefined is undefined");
+}
