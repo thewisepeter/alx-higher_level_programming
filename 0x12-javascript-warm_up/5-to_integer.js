a2a96d9f954d6c4b0769468ab051367286baf497
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-14 23:21:40 +0300

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+const firstArgAsInt = parseInt(args[0]);
+
+if (!isNaN(firstArgAsInt)) {
+  console.log(`My number: ${firstArgAsInt}`);
+} else {
+  console.log("Not a number");
+}
+
