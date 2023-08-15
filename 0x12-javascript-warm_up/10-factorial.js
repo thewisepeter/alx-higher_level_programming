8b4bc115abd9fddccfca9465b258461895f77138
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-15 18:59:52 +0300

    fix on factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 
 function factorial (x) {
-  if (x === 1) {
-    return NaN;
+  if (isNaN(x)) {
+    return 1;
+  } else if (x <= 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
   }
-  return (factorial(x - 1) * x);
 }
+
 const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log(1);
-}
-if (args.length === 1) {
-  const x = parseInt(args[0]);
-  if (!isNaN(x)) {
-    console.log(factorial(x));
-  }
-}
+const input = parseInt(args[0]);
+
+console.log(factorial(input));
