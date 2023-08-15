568d38f3d8eaa7e3551fc136412f138d905d9fe0
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-15 17:26:48 +0300

    semistandard on 4-concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 if (args.length === 2) {
   console.log(`${args[0]} is ${args[1]}`);
 } else if (args.length === 1) {
-  console.log(`${arg[0]} is undefined`);
+  console.log(`${args[0]} is undefined`);
 } else if (args.length === 0) {
   console.log('undefined is undefined');
 }
