3792bc7324abac4dd2f5c6c3c0e3ddb84af8457f
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-23 15:05:03 +0300

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const list = require('./100-data');
+const list = require('./100-data').list;
 
 const newArray = list.map((value, index) => value * index);
 
-console.log('Initial List:', list);
-console.log('New Array:', newArray);
+console.log(list);
+console.log(newArray);
