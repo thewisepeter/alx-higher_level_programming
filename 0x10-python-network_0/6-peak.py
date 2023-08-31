7200fd3f2b1b0778a36bc5f0246cf115c2ee7c7f
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-31 22:02:50 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+finding the peak
+"""
+
+
+def find_peak(list_of_integers):
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
