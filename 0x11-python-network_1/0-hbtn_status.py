be41fa42c6b613abfe456f0dc17ce6a2f441ef6c
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 14:48:28 +0300

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,11 @@ import urllib.request
 url = 'https://alx-intranet.hbtn.io/status'
 try:
     with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
+        content = response.read()
+        content_utf8 = content.decode('utf-8')
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content)
-        print("\t- utf8 content:", content)
+        print("\t- utf8 content:", content_utf8)
 except urllib.error.HTTPError as e:
     print('Error Code: ', e.code)
