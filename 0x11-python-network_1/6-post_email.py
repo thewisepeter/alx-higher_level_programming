7c17fbb5a1761ef6b124bbe9135e8e9cb8af5ebc
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 19:14:39 +0300

    pass an email 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     payload = {'email': sys.argv[2]}
-    
+
     response = requests.post(url, data=payload)
     print(response.text)
