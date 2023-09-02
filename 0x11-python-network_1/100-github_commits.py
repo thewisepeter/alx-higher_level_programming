1a3179089e74d174355cb3c8154954373b130c01
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-03 02:01:32 +0300

    last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    repository_name, owner_name = sys.arg[1], sys.arg[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            owner_name, repository_name)
+    response = requests.get(url)
+    commits = response.json()
+    print(commits)
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
