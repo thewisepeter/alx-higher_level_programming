c93d7cd127d7e5fd7cbe32a7292f9c07e675e581
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 16:35:10 +0300

    added main line

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,7 @@ displays the value of the X-Request-Id variable
 import sys
 import urllib.request
 
+if __name__ == "__main__":
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     headers = response.info()
