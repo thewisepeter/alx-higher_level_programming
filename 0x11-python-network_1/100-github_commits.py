1c2ed2bb00a8708855f2c394fc6c3c2e81f6eeae
thewisepeter <rempeterwisdom@gmail.com>
2023-09-02 16:04:12 -0700

    removed extra print statement

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,11 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    repository_name, owner_name = sys.arg[1], sys.arg[2]
+    repository_name, owner_name = sys.argv[1], sys.argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(
             owner_name, repository_name)
     response = requests.get(url)
     commits = response.json()
-    print(commits)
     try:
         for i in range(10):
             print("{}: {}".format(
