189fe4633968b6adeb4320911d15005a80982baa
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 17:25:24 +0300

    run only as main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 url = 'https://alx-intranet.hbtn.io/status'
-try:
+
+if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         content = response.read()
         content_utf8 = content.decode('utf-8')
@@ -12,5 +13,3 @@ try:
         print("\t- type:", type(content))
         print("\t- content:", content)
         print("\t- utf8 content:", content_utf8)
-except urllib.error.HTTPError as e:
-    print('Error Code: ', e.code)
