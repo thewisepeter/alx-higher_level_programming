1a48c534b4db846728f5e7d2afbfb2ac5e235396
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 16:15:42 +0300

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in a URLsends a request to the URL
+displays the value of the X-Request-Id variable
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = {'email': email}
+data = urllib.parse.urlencode(data).encode('utf-8')
+
+with urllib.request.urlopen(url, data=data) as response:
+    content = response.read().decode('utf-8')
+    print(content)
