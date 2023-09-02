661cc0e01d9c8f7f2b063d795cef09991c372413
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 17:16:56 +0300

    run only as main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,9 @@ displays the value of the X-Request-Id variable
 import sys
 import urllib.request
 
-url = sys.argv[1]
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as response:
-    headers = dict(response.headers)
-    print(headers.get("X-Request-Id")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        headers = dict(response.headers)
+        print(headers.get("X-Request-Id")
