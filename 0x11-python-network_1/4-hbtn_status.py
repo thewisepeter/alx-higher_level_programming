259b61fd6032417824584c696e265cf47f3a68ac
Peter Ochan <rempeterwisdom@gmail.com>
2023-09-02 18:30:47 +0300

    switched texts for read

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import requests
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    content = response.text()
+    content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
