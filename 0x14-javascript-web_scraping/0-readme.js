063e9361730511fb87d5d9eeb84e2ec4726dbabf
thewisepeter <rempeterwisdom@gmail.com>
2023-09-26 07:25:27 -0700

    semisatndard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,6 @@
 const fs = require('fs');
 const filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
-	if (err) console.log(err);
-	else console.log(data.toString());
+  if (err) console.log(err);
+  else console.log(data.toString());
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,5 +5,5 @@ const fs = require('fs');
 const filename = process.argv[2];
 const text = process.argv[3];
 fs.writeFile(filename, text, 'utf-8', (err) => {
-	if (err) console.log(err);
+  if (err) console.log(err);
 });
