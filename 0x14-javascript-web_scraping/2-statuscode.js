c007c8ff570da27fc67c9d37e7662e17ebcbcf11
thewisepeter <rempeterwisdom@gmail.com>
2023-09-27 10:18:48 -0700

    semistandard fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response) => {
-	if (err) console.log(err);
-	else console.log('code:', response.statusCode);
+  if (err) console.log(err);
+  else console.log('code:', response.statusCode);
 });
