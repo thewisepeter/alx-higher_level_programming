269c29f7e05b2a9b8f62f569cb1f016695638049
Peter Ochan <rempeterwisdom@gmail.com>
2023-10-04 21:26:17 +0300

    update on 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,3 @@
-window.onload = function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-    $('div#hello').text(data.hello);
-  });
-};
+$.get("https://fourtonfish.com/hellosalut/?lang=fr", function(data){
+    $("DIV#hello").text(data.hello);
+});
