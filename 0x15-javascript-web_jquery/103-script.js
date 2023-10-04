0ce1f99d71a70c453155257ef3e1906d5a82e9f4
Peter Ochan <rempeterwisdom@gmail.com>
2023-10-04 21:51:46 +0300

    script that translates hello

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+'use strict';
+$(() => {
+  const translateHello = () => {
+    const translateUrl = 'https://fourtonfish.com';
+    const code = $('INPUT#language_code').val();
+
+    // For language codes
+    // see https://www.loc.gov/standards/iso639-2/php/code_list.php
+    $.get(`${translateUrl}/hellosalut/?lang=${code}`, (data, status) => {
+      $('DIV#hello').html(data.hello);
+    });
+  };
+
+  $('INPUT#btn_translate').click(translateHello);
+  $('INPUT#language_code').keydown((ev) => {
+    if (ev.key === 'Enter') translateHello();
+  });
+});
